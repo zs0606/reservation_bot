a4d596f9468d738934effe6eabd1a7d861f970ef
Shahid <shahid.sarker@gmail.com>
2019-07-12 20:54:07 -0400

    test out sms

--- server/routes/reservations.js
+++ server/routes/reservations.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
 
 /* GET users listing. */
 router.get("/", function(req, res, next) {
@@ -8,6 +9,15 @@ router.get("/", function(req, res, next) {
     { id: 1, name: "Eli Ferdinand, party of 2 @ 8pm" },
     { id: 2, name: "Cecilija Frantzisko, party of 5 @ 3pm" }
   ]);
+});
+
+router.post("/sms", (req, res) => {
+  const twiml = new MessagingResponse();
+
+  twiml.message("The Robots are coming! Head for the hills!");
+
+  res.writeHead(200, { "Content-Type": "text/xml" });
+  res.end(twiml.toString());
 });
 
 module.exports = router;
