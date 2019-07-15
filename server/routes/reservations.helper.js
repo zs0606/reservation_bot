f0c8acd32b3080cc2f4024b4407c6de0fef2a6e0
Shahid <shahid.sarker@gmail.com>
2019-07-14 23:16:18 -0400

    refactor date time format

--- server/routes/reservations.helper.js
+++ server/routes/reservations.helper.js
@@ -1,44 +1,96 @@
-// check if the reservation sms format is in the format of "Name, Date (yyyy-mm-dd), Time (hh:mm)"
-const reservationInfo = (reservationMessage) => {
-    const reservationInfoArray = reservationMessage.split(',').map(info => info.trim());
-    return reservationInfoArray;
+// Takes a message in the format 'NAME MM-DD HHpm' and returns an array of info
+const parseRequestBody = requestBody => {
+  return requestBody.split(" ");
 };
 
-const makeDate = (date, time) => {
-    const reservationDate = new Date(`${date}T${time}:00`);
-    return reservationDate;
+const parseDateTime = messageArray => {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+
+  const [month, date] = messageArray[1].split("-").map(int => parseInt(int));
+
+  const time = parseInt(messageArray[2].replace(/\D/g, ""));
+  const parsedDate = new Date(currentYear, month - 1, date, time + 12);
+  return parsedDate;
 };
 
-const validReservationRequest = (reservationInfoArr) => {
-    if (reservationInfoArr.length !== 3) {
-        //check if the reservationInfoArr has 3 elements
-        return false;
-    }
-
-    const reservationDate = makeDate(reservationInfoArr[1], reservationInfoArr[2]);
-    const currentDate = new Date();
-    if (reservationDate == 'Invalid Date') {
-        //check if the 2nd input is a valid date
-        return false;
-    } else if (reservationDate < currentDate) {
-        //check if the reservation date and time pasted today
-        return false;
-    } else if (reservationDate.getHours() < 1 || reservationDate.getHours() >= 9) {
-        //check if the request time is between 1pm and 9pm (store opening between 1pm to 10pm, 1 hour of durations)
-        return false;
-    }
-    return true;
+const validateReservation = dateTime => {
+  const currentDate = new Date();
+  if (dateTime == "Invalid Date") {
+    return false;
+  } else if (dateTime < currentDate) {
+    //check if the reservation date and time past today
+    return false;
+  } else if (dateTime.getHours() < 13 || dateTime.getHours() > 21) {
+    //check if the request time is between 1pm and 9pm (store opening between 1pm to 10pm, 1 hour duration)
+    return false;
+  }
+  return true;
 };
 
-// create a reservation data object
-const createReservation = (msg, reservationInfoArr) => {
-    const reservationDate = makeDate(reservationInfoArr[1], reservationInfoArr[2]);
-    return { id: msg.SmsMessageSid,
-             name: reservationInfoArr[0],
-             dateTime: reservationDate,
-             phoneNumber: msg.From,
-             rawMessage: msg.Body };
+const reservationMaker = twilioReq => {
+  const messageArray = parseRequestBody(twilioReq.Body);
+
+  const reservationDate = parseDateTime(messageArray);
+
+  if (validateReservation(reservationDate)) {
+    const reservationObject = {
+      id: Date.now(),
+      name: messageArray[0],
+      dateTime: reservationDate,
+      phoneNumber: twilioReq.From,
+      rawMessage: twilioReq,
+      createdAt: new Date()
+    };
+
+    return reservationObject;
+  } else {
+    return null;
+  }
 };
 
-module.exports = {createReservation, validReservationRequest, reservationInfo};
+// // check if the reservation sms format is in the format of "Name, Date (yyyy-mm-dd), Time (hh:mm)"
+// const reservationInfo = (reservationMessage) => {
+//     const reservationInfoArray = reservationMessage.split(',').map(info => info.trim());
+//     return reservationInfoArray;
+// };
 
+// const makeDate = (date, time) => {
+//     const reservationDate = new Date(`${date}T${time}:00`);
+//     return reservationDate;
+// };
+
+// const validReservationRequest = (reservationInfoArr) => {
+//     if (reservationInfoArr.length !== 3) {
+//         //check if the reservationInfoArr has 3 elements
+//         return false;
+//     }
+
+//     const reservationDate = makeDate(reservationInfoArr[1], reservationInfoArr[2]);
+//     const currentDate = new Date();
+//     if (reservationDate == 'Invalid Date') {
+//         //check if the 2nd input is a valid date
+//         return false;
+//     } else if (reservationDate < currentDate) {
+//         //check if the reservation date and time pasted today
+//         return false;
+//     } else if (reservationDate.getHours() < 1 || reservationDate.getHours() >= 9) {
+//         //check if the request time is between 1pm and 9pm (store opening between 1pm to 10pm, 1 hour of durations)
+//         return false;
+//     }
+//     return true;
+// };
+
+// // create a reservation data object
+// const createReservation = (msg, reservationInfoArr) => {
+//     const reservationDate = makeDate(reservationInfoArr[1], reservationInfoArr[2]);
+//     return { id: msg.SmsMessageSid,
+//              name: reservationInfoArr[0],
+//              dateTime: reservationDate,
+//              phoneNumber: msg.From,
+//              rawMessage: msg.Body };
+// };
+
+// module.exports = {createReservation, validReservationRequest, reservationInfo};
+
+module.exports = { reservationMaker };
--- server/routes/reservations.js
+++ server/routes/reservations.js
@@ -1,28 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
-const {reservationInfo, validReservationRequest, createReservation} = require('./reservations.helper');
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
+const { reservationMaker } = require("./reservations.helper");
 const database = [];
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res, next) {
   // res.send('respond with a resource');
   res.json(database);
 });
 
 /* POST user request reservation through message and create new reservation*/
-router.post('/sms', (req, res, next) => {
+router.post("/sms", (req, res, next) => {
   const twiml = new MessagingResponse();
-  const newReservationInfo = reservationInfo(req.body.Body);
-  if (validReservationRequest(newReservationInfo)) {
-    twiml.message('Your reservation was successful');
-    const newReservation = createReservation(req.body, newReservationInfo);
-    database.push(newReservation);
+  // console.log(req.body);
+  // const newReservationInfo = reservationInfo(req.body.Body);
+  // reservationMaker(req.body);
+  const reservation = reservationMaker(req.body);
+  if (reservation) {
+    // const newReservation = createReservation(req.body, newReservationInfo);
+    database.push(reservation);
+    twiml.message("Your reservation was successful");
   } else {
-    twiml.message('Your reservation was invalid, please request with "Name, Date (yyyy-mm-dd), Time (hh:mm) in 12 hours standard time", starting from today from 1pm to 9pm');
+    twiml.message(
+      'Your reservation was invalid, please request with "Name Date [mm-dd] Time [hh]", between 1pm and 9pm'
+    );
   }
 
-  res.writeHead(200, { 'Content-Type': 'text/xml' });
+  res.writeHead(200, { "Content-Type": "text/xml" });
   res.end(twiml.toString());
 });
 
